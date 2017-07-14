90b4b48933f895ccf81cbeec692951159b5ac4f9
Viktor Noskin <vitek2012rus@gmail.com>
2017-07-14 15:39:45 +0300

    add MainActivity

---
+++ app/src/main/java/ru/srtdevs/weather/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.srtdevs.weather;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Виктор on 14.07.2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
