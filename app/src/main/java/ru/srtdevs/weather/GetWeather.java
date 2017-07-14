097eca91f69b2bc81cea6969c3bb36fa454102d4
Viktor Noskin <vitek2012rus@gmail.com>
2017-07-14 16:00:45 +0300

    add GetWeather class

---
+++ app/src/main/java/ru/srtdevs/weather/GetWeather.java
@@ -0,0 +1,15 @@
+package ru.srtdevs.weather;
+
+/**
+ * Created by Виктор on 14.07.2017.
+ */
+
+public class GetWeather {
+    //api key для запросов к api openweathermap
+    private static final String ApiKey = "0336fafc5128270fc1d1c40615db4df4";
+
+    //метод для получения информации о городе
+    protected void getCityInfo(String City){
+        
+    }
+}
--- app/src/main/java/ru/srtdevs/weather/MainActivity.java
+++ app/src/main/java/ru/srtdevs/weather/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
